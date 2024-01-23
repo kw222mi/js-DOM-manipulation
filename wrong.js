420a5ec4c47f3d575140826e88e14d75452d90b2
Therese Weidenstedt <kw222mi@student.lnu.se>
2024-01-23 17:55:48 +0100

    finnish restyling the page

--- wrong.js
+++ wrong.js
@@ -11,31 +11,67 @@ let logoText = document.querySelector('.logo-text')
 logoText.style.color = "black"
 
 //Ändra align på header
-var header = document.querySelector('header')
+let header = document.querySelector('header')
 header.style.alignItems = 'flex-start'
 header.style.right = 'auto'
 
-
-//Change the color of the border at the bottom
+// Change the color of the border at the bottom
 header.style.borderBottomColor = 'lightgray'
 
+// Change the name of the recipe
+let recipeName = document.getElementById('recipe-name')
+recipeName.innerText= "Frozen Cheescake"
+
+// Fix the clock by adding a class
+let iconElement = document.querySelector('div > span')
+iconElement.classList.add('material-icons')
+
+// Change the estimated time
+let time = document.querySelector('.time')
+time.innerText= "60+ min"
+
+// Change the src path of the image
+let img = document.querySelector('img')
+const newSrc = "assets/frozen-cheesecake-slice.jpg";
+img.setAttribute("src", newSrc)
+
+//Fix bg-color of ingredients
+let ingredients = document.querySelector('.ingredients-container')
+ingredients.style.backgroundColor = '#f9f9f9'
+
+// Fix the ingredients list
+let ingredientsListBottom = document.querySelector('.ingredients-list-bottom')
+
+let listItem1 = document.createElement('li')
+let listItem2 = document.createElement('li')
+listItem1.textContent = '15st digestivekex'
+listItem2.textContent = 'Lite smör'
+
+ingredientsListBottom.innerHTML = ''
+ingredientsListBottom.appendChild(listItem1)
+ingredientsListBottom.appendChild(listItem2)
+
+// Change the third ingredient
+let ingredientsListPaste = document.querySelector('.ingredients-list-paste')
+let thirdIngredient = ingredientsListPaste.children[2]
+thirdIngredient.textContent = '3 tsk vanlijsocker'
+
+// Add missing ingredient
+let missingIngredient = document.createElement('li')
+missingIngredient.textContent = '400g naturell philadelphiaost'
+ingredientsListPaste.appendChild(missingIngredient)
+
+// Remove shadow
+let instructionsHeading = document.querySelector('.instructions')
+instructionsHeading.style.boxShadow = 'none'
+
+// Change wrong instructions 
+let instructionsList = document.querySelector('.instructions-list');
+let listItemNumber2 = instructionsList.children[1];
+let listItemNumber9 = instructionsList.children[8];
+listItemNumber2.textContent = 'Separera ägggulor och äggvitor. Äggvitorna lägger du i en stor bunke, äggulorna i en liten bunke.';
+listItemNumber9.textContent = 'Ställ in i frysen över natten.';
 
 
 
 }
-
-
-/*
-The header has a border at the bottom, but it has the wrong color. Change it do the correct one.
-The recipe name is wrong, change it to the correct one.
-The clock icon beneath the recipe name has disappeared and been replaced by a text instead. This can be fixed by adding a class to that element.
-The estimated time of the recipe is also incorrect. Change it to the correct time estimation.
-The src path to the image is wrong, or atleast it's showing the wrong image. Change it to the correct one. The available images can be found in the assets folder.
-The background color of the ingredients list container is wrong. Fix it.
-The ingredients are divided in to two parts, one for the bottom and one for the paste. In the list of the ingredients to the bottom, there is a text instead of two list items. Remove the text and add those two list items.
-The third ingredient in the list of ingredients to the paste is wrong. Change that specific ingredient to the correct one.
-There is also a missing ingredient in the list of ingredients to the paste. Look and see what it is and add that one the the end of the list.
-The text "Instructions" to the left, beneath the image, has some shadow styling applied to it. Remove that styling.
-Two list elements of the list of instructions are incorrect. Find them and change them to the correct ones.
-
-*/
